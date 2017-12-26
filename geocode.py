330b10ab563d63e435fbd0f4f06b4f078d8df2b1
AmitojGill <amitoj.s.gill@gmail.com>
2017-12-26 13:54:43 -0800

    Add getGeocodeLocation() method

--- geocode.py
+++ geocode.py
@@ -1,2 +1,14 @@
 import httplib2
 import json
+
+def getGeocodeLocation(inputString):
+	google_api_key = "AIzaSyB5Ag_6XcOch-Et__YJGUL30oTYZtO9lT0"
+	locationString = inputString.replace(" ","+")
+	url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
+	h = httplib2.Http()
+	response, content = h.request(url, 'GET')
+	result = json.loads(content)
+	print "response header: %s \n \n" % response
+	return result
+
+

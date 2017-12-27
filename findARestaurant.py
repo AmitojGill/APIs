998ada690b6e13c9fd7f62c17f6147e62b6246b1
AmitojGill <amitoj.s.gill@gmail.com>
2017-12-27 14:44:12 -0800

    Extract first_restaurant from result

--- findARestaurant.py
+++ findARestaurant.py
@@ -14,12 +14,12 @@ foursquare_client_secret = "OUZOBXI133ETHMNVCHVTYXME4AKHPZMHTU4IQNHM3K4LC0WX"
 def findARestaurant(mealType,location):
 	#1. Use getGeocodeLocation to get the latitude and longitude coordinates of the location string.
 	latitude, longitude = getGeocodeLocation(location)
-	print "="*len(location)
-	print location
-	print "="*len(location)
-	print latitude
-	print longitude
-
+	#print "="*len(location)
+	#print location
+	#print "="*len(location)
+	#print latitude
+	#print longitude
+	
 	#2.  Use foursquare API to find a nearby restaurant with the latitude, longitude, and mealType strings.
 	#HINT: format for url will be something like https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
 	url = ('https://api.foursquare.com/v2/venues/search?client_id=%s&client_secret=%s&v=20130815&ll=%s,%s&query=%s&limit=1' %(foursquare_client_id, foursquare_client_secret,latitude,longitude,mealType))
@@ -28,9 +28,12 @@ def findARestaurant(mealType,location):
 	response, content = h.request(url,'GET')
 	result = json.loads(content)
 	print result
-
+	print "=" * 100
 	#3. Grab the first restaurant
+	first_restaurant = result["response"]["venues"][0]["name"]
+	print first_restaurant
 	#4. Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
+
 	#5. Grab the first image
 	#6. If no image is available, insert default a image url
 	#7. Return a dictionary containing the restaurant name, address, and image url	

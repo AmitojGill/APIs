4a16abf7050d56771b76cf7f6d73fc5302a0b0a6
Amitoj-Gill <amitoj.s.gill@gmail.com>
2018-01-08 21:29:32 -0800

    Add hash_password method

--- bagelShop/models.py
+++ bagelShop/models.py
@@ -13,7 +13,10 @@ class User(Base):
 	id = Column(Integer, primary_key=True)
 	username = Column(String(32), index = True)
 	password_hash = Column(String(64))
-	
+
+	def hash_password(self, password):
+		self.password_hash = pwd_context.encrypt(password)
+
 
 class Bagel(Base):
 	__tablename__ = 'bagel'

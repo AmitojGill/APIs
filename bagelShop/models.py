f572a6607a5079eb114945b01c576b7f69d88c35
Amitoj-Gill <amitoj.s.gill@gmail.com>
2018-01-08 21:24:12 -0800

    Crate User Class

--- bagelShop/models.py
+++ bagelShop/models.py
@@ -7,6 +7,14 @@ Base = declarative_base()
 
 #ADD YOUR USER MODEL HERE
 
+class User(Base):
+	__tablename__ = 'user'
+
+	id = Column(Integer, primary_key=True)
+	username = Column(String(32), index = True)
+	password_hash = Column(String(64))
+	
+
 class Bagel(Base):
 	__tablename__ = 'bagel'
 	id = Column(Integer, primary_key=True)

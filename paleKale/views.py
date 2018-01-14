2244cf35a4004e8dc8f639692fae76198cf9f246
Amitoj-Gill <amitoj.s.gill@gmail.com>
2018-01-14 13:43:44 -0800

    Add sqltie enigne and session

--- paleKale/views.py
+++ paleKale/views.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from models import Base, User
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+engine = create_engine('sqlite:///usersWithOauth.db')
+DBsession = sessionmaker(bind=engine)
+session = DBsession()
+
+
 
 app = Flask(__name__)
+
+
 
 if __name__ == '__main__':
 	app.debug = True

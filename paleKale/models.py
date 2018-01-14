1e7c4f3fe199a0565b3476944ab9d2e9f301b3ed
Amitoj-Gill <amitoj.s.gill@gmail.com>
2018-01-14 13:35:54 -0800

    Add generate_auth_token() method

--- paleKale/models.py
+++ paleKale/models.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
 import random, string 
-
+from sqlalchemy.orm import relationship, sessionmaker
+from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 
 Base = declarative_base()
@@ -22,6 +23,10 @@ class User(Base):
 
 	def verify_password(self, password):
 		return pwd_context.verify(password, self.password_hash)
+
+	def generate_auth_token(self, expiration=600):
+		s = Serializer(secret_key, expires_in = expiration)
+		return s.dumps({'id': self.id})
 
 
 engine = create_engine('sqlite:///usersWithOAuth.db')

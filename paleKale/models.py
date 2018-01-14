0e83622b6bbfce35ea50ae504e9fc5b39b19fffc
Amitoj-Gill <amitoj.s.gill@gmail.com>
2018-01-14 13:16:04 -0800

    Implement hash_password() method

--- paleKale/models.py
+++ paleKale/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
 
 
 Base = declarative_base()
@@ -12,6 +13,9 @@ class User(Base):
 	picture = Column(String)
 	email = Column(String)
 	password_hash = Column(String(64))
+
+	def hash_password(self, password):
+		self.password_hash = pwd_context.encrypt(password)
 
 engine = create_engine('sqlite:///usersWithOAuth.db')
 
